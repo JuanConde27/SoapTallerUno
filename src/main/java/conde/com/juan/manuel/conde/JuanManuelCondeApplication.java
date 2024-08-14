3e79fa128eb6b17d1caf7090389b80e054b8ec7d
Juan Manuel Conde Aldana <condejuan27@gmail.com>
2024-08-13 20:04:51 -0500

    first commit

---
+++ src/main/java/conde/com/juan/manuel/conde/JuanManuelCondeApplication.java
@@ -0,0 +1,13 @@
+package conde.com.juan.manuel.conde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanManuelCondeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanManuelCondeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/conde/com/juan/manuel/conde/JuanManuelCondeApplicationTests.java
@@ -0,0 +1,13 @@
+package conde.com.juan.manuel.conde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanManuelCondeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
